830925a4ef4683e57dc28e95dc1524d530b799a1
Zsolt Keseru <zsolooo@gmail.com>
2018-04-26 21:01:28 +0200

    update

--- src/main/scala/banzaicloud/SparkPi.scala
+++ src/main/scala/banzaicloud/SparkPi.scala
@@ -22,6 +22,7 @@ object SparkPi {
       if (x * x + y * y <= 1) 1 else 0
     }.reduce(_ + _)
     println("Pi is roughly " + 4.0 * count / (n - 1))
+    println("Pi is roughly " + 4.0 * count / (n - 1))
     spark.stop()
   }
 }
